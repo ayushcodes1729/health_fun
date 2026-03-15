edd5ce4bced10b7eaa5231defbe94d6e432a3047
Ayush Kumar <ayushtiwari0803@gmail.com>
2026-03-15 17:22:14 +0530

    feat:Init health_data instruction

---
+++ programs/health_fun/src/instructions/initialize_health_data.rs
@@ -0,0 +1,46 @@
+use anchor_lang::prelude::*;
+
+use crate::HealthData;
+
+#[derive(Accounts)]
+pub struct InitializeHealthData<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = 8 + HealthData::INIT_SPACE,
+        seeds = [b"health" , user.key().as_ref()],
+        bump
+    )]
+    pub health_data: Account<'info, HealthData>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> InitializeHealthData<'info> {
+    pub fn initialize_health_data(
+        &mut self,
+        steps: u32,
+        sleep_hours: u8,
+        gym: bool,
+        last_oracle: Pubkey,
+    ) -> Result<()> {
+
+        let current_timestamp = Clock::get()?.unix_timestamp;
+
+        let epoch_day = current_timestamp.checked_div(86400).expect("Epoch day after division with 86400");
+
+        self.health_data.set_inner(HealthData {
+            user: *self.user.key,
+            last_sync_timestamp: current_timestamp,
+            epoch_day: epoch_day as u16,
+            steps,
+            sleep_hours,
+            gym,
+            last_oracle,
+        });
+        Ok(())
+    }
+}
--- programs/health_fun/src/instructions/mod.rs
+++ programs/health_fun/src/instructions/mod.rs
@@ -1,3 +1,5 @@
 pub mod initialize_config;
-
 pub use initialize_config::*;
+
+pub mod initialize_health_data;
+pub use initialize_health_data::*;
